736045f45343d292f4cd2c244c15cc00b1702dc5
Kieran Boyle <norcal.kieran@gmail.com>
2023-09-12 11:59:57 -0700

    fix(tests): update test snapshots

--- src/action.js
+++ src/action.js
@@ -2,75 +2,75 @@ import Tooltip from './action-tooltip.svelte';
 
 export const tooltip = (element, props) => {
 
-	let component = null;
-	let title = element.getAttribute('title');
-	let action = props?.action || element.getAttribute('action') || 'hover';
+  let component = null;
+  let title = element.getAttribute('title');
+  let action = props?.action || element.getAttribute('action') || 'hover';
 
-	if (title) {
-		element.removeAttribute('title');
+  if (title) {
+    element.removeAttribute('title');
 
-		props = {
-			content: title,
-			...props
-		}
-	}
+    props = {
+      content: title,
+      ...props
+    }
+  }
 
-	const onClick = () => {
-		if (component) {
-			onMouseLeave();
-		} else {
-			onMouseEnter();
-		}
-	};
+  const onClick = () => {
+    if (component) {
+      onMouseLeave();
+    } else {
+      onMouseEnter();
+    }
+  };
 
-	const onMouseEnter = () => {
-		if (!component) {
-			component = new Tooltip({
-				target: element,
-				props
-			});
-		}
-	};
+  const onMouseEnter = () => {
+    if (!component) {
+      component = new Tooltip({
+        target: element,
+        props
+      });
+    }
+  };
 
-	const onMouseLeave = () => {
-		if (component) {
-			component.$destroy();
-			component = null;
-		}
-	};
+  const onMouseLeave = () => {
+    if (component) {
+      component.$destroy();
+      component = null;
+    }
+  };
 
-	const addListeners = () => {
-		if (element !== null) {
-			removeListeners();
+  const addListeners = () => {
+    if (element !== null) {
+      removeListeners();
 
-			if (action === 'click') {
-				element.addEventListener('click', onClick);
-			} else {
-				element.addEventListener('mouseenter', onMouseEnter);
-				element.addEventListener('mouseleave', onMouseLeave);
-			}
-		}
-	}
+      if (action === 'click') {
+        element.addEventListener('click', onClick);
+      } else {
+        element.addEventListener('mouseenter', onMouseEnter);
+        element.addEventListener('mouseleave', onMouseLeave);
+      }
+    }
+  }
 
-	const removeListeners = () => {
-		if (element !== null) {
-			element.removeEventListener('click', onClick);
-			element.removeEventListener('mouseenter', onMouseEnter);
-			element.removeEventListener('mouseleave', onMouseLeave);
-		}
-	};
+  const removeListeners = () => {
+    if (element !== null) {
+      element.removeEventListener('click', onClick);
+      element.removeEventListener('mouseenter', onMouseEnter);
+      element.removeEventListener('mouseleave', onMouseLeave);
+    }
+  };
 
-	addListeners();
+  addListeners();
 
-	element.style.position = 'relative';
+  element.style.position = 'relative';
 
-	return {
-		destroy() {
-			removeListeners();
+  return {
+    destroy() {
+      removeListeners();
 
-			if (title) {
-				element.setAttribute('title', title);
-			}
-		}
-	};
+      if (title) {
+        element.setAttribute('title', title);
+      }
+    }
+  };
 }
