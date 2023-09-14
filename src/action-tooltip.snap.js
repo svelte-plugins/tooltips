bd106d73bd850ef0c992edabbd60bb8ad7d668a3
Kieran Boyle <norcal.kieran@gmail.com>
2023-09-13 21:16:41 -0700

    tests(snapshots): udpate snapshots with the latest template changes

--- src/action-tooltip.snap.js
+++ src/action-tooltip.snap.js
@@ -5,11 +5,12 @@ exports[`Components: Tooltip should render the component 1`] = `
   <div>
     <div
       class="tooltip animation-null top"
-      style="min-width: 200px; max-width: 200px; text-align: left;"
+      style="left: 0px; min-width: 200px; max-width: 200px; text-align: left; top: 0px;"
     >
       Hello World!
       
     </div>
+    
   </div>
 </body>
 `;
