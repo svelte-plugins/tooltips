349cf5f16f12e55ab20bd499ec548564e33fa728
Inzanez <inzanez@gmx.net>
2024-12-10 09:52:44 +0100

    Add 'svelteTesting'

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,18 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { svelteTesting } from '@testing-library/svelte/vite'
 
 export default defineConfig({
-  plugins: [svelte({ emitCss: false })],
+  plugins: [svelte({ emitCss: false }), svelteTesting()],
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: ['./vitest.setup.js'],
     testTimeout: 20000,
-    alias: [
-      {
-        find: /^svelte$/,
-        replacement: 'svelte/internal'
-      }
-    ],
     include: ['./src/**/*.test.js'],
+    alias: [
+
+    ],
     resolveSnapshotPath: (testPath, snapExtension) => testPath.replace(/\.test\.([tj]s?)/, `${snapExtension}.$1`)
   }
 });
